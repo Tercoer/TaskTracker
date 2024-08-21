e0bf80ecc3eb86abc13540470583c8c30dd64e30
Jorge Luis Soltero Rivera <jorgejs.tech@hotmail.com>
2024-08-20 19:57:11 -0600

    add, update, list, delete

---
+++ TaskTracker/Task Tracker Net/Model/TaskTracker.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Task_Tracker_Net.Model {
+    public class TaskTracker {
+
+        public static bool CreateTask(ModelTask task) {
+            string currentDirectory = Directory.GetCurrentDirectory();// + "\\tasks\\tasks.json";
+            string jsonPath = Path.GetDirectoryName(Path.GetDirectoryName(currentDirectory)) + "\\tasks\\tasks.json";
+           
+            bool fileExist = File.Exists(jsonPath);
+            List<ModelTask> tasks = new List<ModelTask>();
+
+            if(fileExist) {
+                string file = File.ReadAllText(jsonPath);
+                tasks = JsonSerializer.Deserialize<List<ModelTask>>(file);
+            }
+
+            task.id = tasks.Max(t => t.id) + 1;
+            tasks.Add(task);
+            string serialized = JsonSerializer.Serialize(tasks);
+            File.WriteAllText(jsonPath, serialized);
+
+            return true;
+        }
+
+        public static bool UpdateTask(ModelTask task) {
+            string currentDirectory = Directory.GetCurrentDirectory();// + "\\tasks\\tasks.json";
+            string jsonPath = Path.GetDirectoryName(Path.GetDirectoryName(currentDirectory)) + "\\tasks\\tasks.json";
+
+            if(!File.Exists(jsonPath))
+                return false;
+
+            string file = File.ReadAllText(jsonPath);
+
+            List<ModelTask> tasks = JsonSerializer.Deserialize<List<ModelTask>>(file);
+
+            if(tasks == null || tasks.Count == 0) 
+                return false;
+            
+            int index = tasks.FindIndex(x => x.id == task.id);
+
+            if(index < 0)
+                return false;
+
+            tasks[index] = task;
+
+            string serialized = JsonSerializer.Serialize(tasks);
+            File.WriteAllText(jsonPath, serialized);
+
+            return true;
+        }
+
+        public static bool RemoveTask(ModelTask task) {
+            string currentDirectory = Directory.GetCurrentDirectory();// + "\\tasks\\tasks.json";
+            string jsonPath = Path.GetDirectoryName(Path.GetDirectoryName(currentDirectory)) + "\\tasks\\tasks.json";
+
+            if(!File.Exists(jsonPath))
+                return false;
+
+            string file = File.ReadAllText(jsonPath);
+
+            List<ModelTask> tasks = JsonSerializer.Deserialize<List<ModelTask>>(file);
+
+            if(tasks == null || tasks.Count == 0)
+                return false;
+
+            int index = tasks.FindIndex(x => x.id == task.id);
+
+            if(index < 0)
+                return false;
+
+            tasks.RemoveAt(index);
+
+            string serialized = JsonSerializer.Serialize(tasks);
+            File.WriteAllText(jsonPath, serialized);
+
+            return true;
+        }
+
+        public static List<ModelTask> GetTasks(ModelTask task) {
+            string currentDirectory = Directory.GetCurrentDirectory();// + "\\tasks\\tasks.json";
+            string jsonPath = Path.GetDirectoryName(Path.GetDirectoryName(currentDirectory)) + "\\tasks\\tasks.json";
+            
+            if(!File.Exists(jsonPath))
+                return null;
+
+            string file = File.ReadAllText(jsonPath);
+
+            List<ModelTask> tasks = JsonSerializer.Deserialize<List<ModelTask>>(file);
+
+            return tasks;
+        }
+
+    }
+
+    public enum TaskStatus {
+        TODO = 0,
+        IN_PROGRESS = 1, 
+        DONE = 2
+    }
+
+    public class ModelTask {
+        /*
+            id: A unique identifier for the task
+            description: A short description of the task
+            status: The status of the task (todo, in-progress, done)
+            createdAt: The date and time when the task was created
+            updatedAt: The date and time when the task was last updated
+         */
+        public int id { get; set; } = 0;
+        public string description { get; set; } = "";
+        public TaskStatus taskStatus { get; set; } = TaskStatus.TODO;
+        public DateTime createdAt { get; set; }
+        public DateTime updatedAt { get; set; }
+    }
+}
--- TaskTracker/Task Tracker Net/Program.cs
+++ TaskTracker/Task Tracker Net/Program.cs
@@ -1,7 +1,43 @@
-﻿namespace Task_Tracker_Net {
+﻿using System;
+using System.Collections.Generic;
+using Task_Tracker_Net.Model;
+
+namespace Task_Tracker_Net {
     internal class Program {
+
+        public enum commands {
+            LIST = 0,
+            ADD = 1, 
+            UPDATE = 2,
+            DELETE = 3,
+
+        };
         static void Main(string[] args) {
+            loadMenu();
+        }
+
+        private static void loadMenu() {
+            ModelTask t = new ModelTask();
+            t.createdAt = DateTime.Now;
+            t.taskStatus = TaskStatus.DONE;
+            t.description = "changed by index";
+            //TaskTracker.RemoveTask(t);
+            TaskTracker.CreateTask(t);
+        }
+
+        private static void waitForKeyboard() {
+            Console.WriteLine("Welcome");
+            bool isRunning = true;
+            while(isRunning) {
+
+                switch() {
+
+                }
+
+
+            }
 
         }
+
     }
 }
